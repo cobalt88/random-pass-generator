313770c2fb1f3225b692657bc91b271d70b603f4
cobalt88 <vincentteune@gmail.com>
2022-03-05 19:49:19 -0500

    i broke it

--- logic.js
+++ logic.js
@@ -1,21 +1,7 @@
 // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+ var generateBtn = document.querySelector("#generate");
 // console.log(generateBtn)
 
-// var lowerLetters = [
-//   "a", "b", "c", "d", "e", "f", "g", "h", "i" , "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-// ];
-
-// var upperLetters = [
-//   "A", "B", "C", "D", "E",
-// ]
-
-// var numbers= [
-//   "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-// ];
-// var specialChar = [
-//   " ", "!", '"', "#", "$", "%", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "\\", "^", "_", "`", "{", "|", "}", "~"
-// ];
 
 const myArrayUpper = Array.from(Array(26)).map((e,i) => i + 65);
 const alphabetUpper = myArrayUpper.map((x) => String.fromCharCode(x));
@@ -23,29 +9,58 @@ const alphabetUpper = myArrayUpper.map((x) => String.fromCharCode(x));
 const myArrayLower = Array.from(Array(26)).map((e,i) => i + 97);
 const alphabetLower = myArrayLower.map((x) => String.fromCharCode(x));
 
-console.log(upperLetters)
-console.log(lowerLetters.length);
-console.log(numbers.length);
-console.log(specialChar.length);
-
-
-var passwordLength = window.prompt("How long would you like you password to be?")
-  if(passwordLength < 129 && passwordLength > 7) {
-    window.prompt("Are there any specific character you would prefer to include?")
-  } else {
-    alert("That is an invalid entry. Select a length between 8 and 128")
-  }
-
-  passwordText.value = password;
+const numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+const specChar = ["!", "@", "#", "$", "%", "^", "&","*", "(", ")"]
 
 
 function generatePassword() {
-  var password = passwordObjectGen();
-  console.log(password);
-  console.log(password.passLength);
-  console.log(criteriaAggregate);
+var results = "";
+var numberOfCharacters = window.prompt("How long would you like your password to be?");
+var charQty = parseInt(numberOfCharacters)
+
+  if(charQty < 129 && charQty > 7) { 
+  } else {
+    window.alert("That is an invalid entry. Select a length between 8 and 128")
+    return generatePassword();
+  }
+
+var upperCase = window.confirm("Would you like to include upper case letters?");
+
+// var lowerCase = window.confirm("How about lower case?");
+
+// var numeric  = window.confirm("Include numbers?");
+
+// var specCar = window.confirm("Special Characters?");
+  
+if (upperCase == true) {
+  for (var i = 0; 1 < charQty; i++) {
+    results += alphabetUpper[Math.floor(Math.random()*26)];
+    }
+return results;
+  }
 }
 
-// Add event listener to generate button
+
+// var upperLetters = window.prompt("Would you like to include any upper care letters?")
+//   if (upperLetters = true) {
+//   }
+// }
+
+
+
+// function generatePassword() {
+//   var password = passwordObjectGen();
+//   console.log();
+//   console.log();
+//   console.log();
+// }
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
 generateBtn.addEventListener("click", writePassword);
 
